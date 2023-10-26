c266d66d7fa5450476a2d5e347ee14afe894f043
ericle.costa <ericle.costa@gmail.com>
2023-10-26 10:48:40 -0400

    Alteracao icone

--- src/conexao.php
+++ src/conexao.php
@@ -26,7 +26,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="icon" href="../img/icone.png">
+    <link rel="icon" href="../img/icone_tb_bi.png">
     <title>TB BI</title>
   </head>
   <body>
